b207261a83f4c4add3ec4f754faf8b44d3c1507f
Al Zee <z@alz.ee>
2021-01-20 18:56:54 +0800

    entity vip

---
+++ src/Entity/Vip.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VipRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VipRepository::class)
+ */
+class Vip
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity=User::class, cascade={"persist", "remove"})
+     */
+    private $uid;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $level;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUid(): ?User
+    {
+        return $this->uid;
+    }
+
+    public function setUid(?User $uid): self
+    {
+        $this->uid = $uid;
+
+        return $this;
+    }
+
+    public function getLevel(): ?string
+    {
+        return $this->level;
+    }
+
+    public function setLevel(string $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+}
