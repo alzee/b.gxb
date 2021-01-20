e8b53f0779fa73bf7bfa8cd4e60ddde11481c0fc
Al Zee <z@alz.ee>
2021-01-20 23:34:33 +0800

    no more require label in entity tag/category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -23,7 +23,7 @@ class Category
     private $name;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $label;
 
