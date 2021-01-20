4ec4d7f6c8ad13b0caba081da69c8bf2b6087320
Al Zee <z@alz.ee>
2021-01-20 19:36:38 +0800

    entity bid

---
+++ migrations/Version20210120113447.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210120113447 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE bid (id INT AUTO_INCREMENT NOT NULL, task_id INT NOT NULL, bid INT NOT NULL, position INT NOT NULL, UNIQUE INDEX UNIQ_4AF2B3F38DB60186 (task_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE bid ADD CONSTRAINT FK_4AF2B3F38DB60186 FOREIGN KEY (task_id) REFERENCES task (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE bid');
+    }
+}
---
+++ src/Entity/Bid.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BidRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BidRepository::class)
+ */
+class Bid
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity=Task::class, cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $task;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $bid;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $position;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTask(): ?Task
+    {
+        return $this->task;
+    }
+
+    public function setTask(Task $task): self
+    {
+        $this->task = $task;
+
+        return $this;
+    }
+
+    public function getBid(): ?int
+    {
+        return $this->bid;
+    }
+
+    public function setBid(int $bid): self
+    {
+        $this->bid = $bid;
+
+        return $this;
+    }
+
+    public function getPosition(): ?int
+    {
+        return $this->position;
+    }
+
+    public function setPosition(int $position): self
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+}
