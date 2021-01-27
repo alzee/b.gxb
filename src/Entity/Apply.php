7d533fcc0c1812741d3babb71748d4b224692ba3
Al Zee <z@alz.ee>
2021-01-27 13:08:49 +0800

    expose entity apply/status

--- src/Entity/Apply.php
+++ src/Entity/Apply.php
@@ -4,8 +4,10 @@ namespace App\Entity;
 
 use App\Repository\ApplyRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=ApplyRepository::class)
  */
 class Apply
--- src/Entity/Status.php
+++ src/Entity/Status.php
@@ -4,8 +4,10 @@ namespace App\Entity;
 
 use App\Repository\StatusRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=StatusRepository::class)
  */
 class Status
