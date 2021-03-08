713459c0ff7d97cffc9701bb5c6b052b625808f1
Al Zee <z@alz.ee>
2021-03-08 19:00:27 +0800

    expose entity EquityTrade

--- src/Entity/EquityTrade.php
+++ src/Entity/EquityTrade.php
@@ -4,8 +4,12 @@ namespace App\Entity;
 
 use App\Repository\EquityTradeRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=EquityTradeRepository::class)
  */
 class EquityTrade
