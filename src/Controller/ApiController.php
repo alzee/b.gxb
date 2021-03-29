75c1f75c9b8b8f44fb3e186ef6d9cab0dea1f639
Al Zee <z@alz.ee>
2021-03-29 10:09:38 +0800

    /api/sms

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api", name="api")
+ */
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/sms", name="api")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ApiController.php',
+        ]);
+    }
+}
