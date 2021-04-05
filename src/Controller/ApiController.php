12923256ca860d75be1407138e664b2e3f002393
Al Zee <z@alz.ee>
2021-04-05 19:38:10 +0800

    /api/wxpay

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -9,12 +9,43 @@ use Symfony\Component\Routing\Annotation\Route;
 use AlibabaCloud\SDK\Dysmsapi\V20170525\Dysmsapi;
 use Darabonba\OpenApi\Models\Config;
 use AlibabaCloud\SDK\Dysmsapi\V20170525\Models\SendSmsRequest;
+use GuzzleHttp\Exception\RequestException;
+use WechatPay\GuzzleMiddleware\WechatPayMiddleware;
+use WechatPay\GuzzleMiddleware\Util\PemUtil;
+use GuzzleHttp\HandlerStack;
 
 /**
  * @Route("/api", name="api")
  */
 class ApiController extends AbstractController
 {
+    /**
+     * @Route("/wxpay", name="_wxpay")
+     */
+    public function wxpay(): Response
+    {
+        // 商户相关配置，
+        $merchantId = '1606036532'; // 商户号
+        $merchantSerialNumber = '58ECB0644D50C6454BE2BDA53BA2422CA20B45A2'; // 商户API证书序列号
+        $merchantPrivateKey = PemUtil::loadPrivateKey('/home/al/cert/wxpay/apiclient_key.pem'); // 商户私钥文件路径
+
+        // 微信支付平台配置
+        $wechatpayCertificate = PemUtil::loadCertificate('/home/al/cert/wxpay/apiclient_cert.pem'); // 微信支付平台证书文件路径
+
+        // 构造一个WechatPayMiddleware
+        $wechatpayMiddleware = WechatPayMiddleware::builder()
+            ->withMerchant($merchantId, $merchantSerialNumber, $merchantPrivateKey) // 传入商户相关配置
+            ->withWechatPay([ $wechatpayCertificate ]) // 可传入多个微信支付平台证书，参数类型为array
+            ->build();
+
+        // 将WechatPayMiddleware添加到Guzzle的HandlerStack中
+        $stack = GuzzleHttp\HandlerStack::create();
+        $stack->push($wechatpayMiddleware, 'wechatpay');
+
+        // 创建Guzzle HTTP Client时，将HandlerStack传入，接下来，正常使用Guzzle发起API请求，WechatPayMiddleware会自动地处理签名和验签
+        $client = new GuzzleHttp\Client(['handler' => $stack]);
+    }
+
     /**
      * @Route("/sms", name="_sms")
      */
