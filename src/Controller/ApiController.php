36d8196dae2047d14bf31aa6a25da457860b27ea
Al Zee <z@alz.ee>
2021-04-08 14:18:34 +0800

    sms vcode as string

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -258,7 +258,7 @@ class ApiController extends AbstractController
         }
 
         return $this->json([
-            'code' => $code,
+            'code' => "$code",
             'phone' => $phone,
             'type' => $type,
             'msg' => $msg
