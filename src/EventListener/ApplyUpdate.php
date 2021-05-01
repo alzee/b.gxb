0e7c44116e46a90bfbfdc71c53c23672d8df81aa
Al Zee <z@alz.ee>
2021-05-01 23:46:23 +0800

    new finance for referrer/ror

--- src/EventListener/ApplyUpdate.php
+++ src/EventListener/ApplyUpdate.php
@@ -12,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\User;
 use App\Entity\Apply;
 use App\Entity\Conifg;
+use App\Entity\Finance;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 
 class ApplyUpdate extends AbstractController
@@ -37,11 +38,24 @@ class ApplyUpdate extends AbstractController
                 $rewardRate = $configRepo->findOneBy(['label' => 'referReward']);
                 $referer->setTopup($referer->getTopup() + ($price * $rewardRate));
                 // new finance for $referer
+                $f1 = new Finance();
+                $f1->setUser($referer);
+                $f1->setAmount($price * $rewardRate);
+                $f1->setType(11);
+                $f1->setNote('任务分销奖励1级 ' . $applicant->getUsername());
+                $em->persist($f1);
+
                 $rOfReferer = $referer->getReferrer();
                 if (!is_null($rOfReferer)) {
                     $rewardRate2 = $configRepo->findOneBy(['label' => 'referReward2']);
                     $rOfReferer->setTopup($rOfReferer->getTopup() + ($price * $rewardRate2));
                     // new finance for $rOfReferer
+                    $f2 = new Finance();
+                    $f2->setUser($rOfReferer);
+                    $f2->setAmount($price * $rewardRate2);
+                    $f2->setType(12);
+                    $f1->setNote('任务分销奖励2级 ' . $applicant->getUsername());
+                    $em->persist($f2);
                 }
             }
             $em->flush();
