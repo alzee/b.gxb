bbca88fff31e03bb8ec4f5f2cb1f2cdc1183b4e1
Al Zee <z@alz.ee>
2021-05-09 11:06:04 +0800

    nth

---
+++ src/Command/proveTimeoutCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class TCommand extends Command
+{
+    protected static $defaultName = 't';
+
+    private $userRepo;
+
+    private $em;
+
+    public function __construct(UserRepository $userRepo, EntityManagerInterface $em)
+    {
+        $this->userRepo = $userRepo;
+        $this->em = $em;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
+
+        if ($arg1) {
+            // $io->note(sprintf('You passed an argument: %s', $arg1));
+            $him = $this->userRepo->getHim($arg1);
+            // $him->setNick('test');
+            // $this->em->flush();
+            $io->success($him->getUsername());
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $ranking = $this->userRepo->ranking();
+
+        dump($ranking);
+        //foreach ($ranking as $u) {
+        //    $io->success($u->getReferrer());
+        //}
+
+
+
+
+        // $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
