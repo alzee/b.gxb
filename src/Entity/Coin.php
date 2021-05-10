014a2657fb0d9747c9bc78850432f2355f48af2b
Al Zee <z@alz.ee>
2021-05-10 23:20:06 +0800

    nth

--- src/Entity/Coin.php
+++ src/Entity/Coin.php
@@ -4,8 +4,10 @@ namespace App\Entity;
 
 use App\Repository\CoinRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=CoinRepository::class)
  */
 class Coin
