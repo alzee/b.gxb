a39e74470ec8b37f2b9416a0461f4861a56506c9
Al Zee <z@alz.ee>
2021-05-10 19:28:51 +0800

    read entity

---
+++ src/Entity/Read.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReadRepository;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=ReadRepository::class)
+ * @ORM\Table(name="`read`")
+ */
+class Read
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=LandPost::class)
+     */
+    private $post;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    public function __construct()
+    {
+        $this->date = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getPost(): ?LandPost
+    {
+        return $this->post;
+    }
+
+    public function setPost(?LandPost $post): self
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
