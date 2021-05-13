535ea73f8ad1ed82bbea34a5dc1864f7f24bf602
Al Zee <z@alz.ee>
2021-05-13 11:22:59 +0800

    nodeNew event to check empty author

---
+++ src/EventListener/NodeNew.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * vim:ft=php et ts=4 sts=4
+ * @author z14 <z@arcz.ee>
+ * @version
+ * @todo
+ */
+
+namespace App\EventListener;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Node;
+use App\Entity\User;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+
+class NodeNew
+{
+    public function prePersist(Node $node, LifecycleEventArgs $event): void
+    {
+        if (is_null($node->getAuthor())) {
+            $em = $event->getEntityManager();
+            $author = $em->getRepository(User::class)->find(4);
+            $node->setAuthor($author);
+        }
+    }
+}
