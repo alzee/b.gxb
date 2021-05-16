d0fbe7eb4f414ab598fc6858b739f64d7f93aaf4
Al Zee <z@alz.ee>
2021-05-16 12:59:02 +0800

    dump config.sql

--- sql/config.sql
+++ sql/config.sql
@@ -1,13 +1,13 @@
--- MariaDB dump 10.19  Distrib 10.4.18-MariaDB, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.33, for Linux (x86_64)
 --
 -- Host: localhost    Database: gxb
 -- ------------------------------------------------------
--- Server version	10.4.18-MariaDB
+-- Server version	5.7.33-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -28,7 +28,7 @@ CREATE TABLE `config` (
   `value` double NOT NULL,
   `label` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `config` (
 
 LOCK TABLES `config` WRITE;
 /*!40000 ALTER TABLE `config` DISABLE KEYS */;
-INSERT INTO `config` VALUES (1,'股权总量',879755,'equity'),(2,'推广一级奖励',0.08,'referReward'),(3,'推广二级奖励',0.02,'referReward2'),(4,'推广GXB奖励',100,'referGXB'),(5,'主城起步价',1,'mainlandMinPrice'),(6,'领地起步价',0.05,'landMinPrice'),(7,'主城最低天数',10,'mainlandMinDays'),(8,'领地最低天数',20,'landMinDays'),(9,'股权每日最大兑换数量',31531644,'maxPerDay'),(10,'股权GXB兑例比率',0.02,'EquityGXBRate'),(11,'股权价格',99,'equityPrice');
+INSERT INTO `config` VALUES (1,'股权总量',14000000000,'equity'),(2,'推广一级奖励',0.08,'referReward'),(3,'推广二级奖励',0.02,'referReward2'),(4,'推广GXB奖励',100,'referGXB'),(5,'主城起步价',1,'mainlandMinPrice'),(6,'分城起步价',0.05,'landMinPrice'),(7,'主城最低天数',1,'mainlandMinDays'),(8,'分城最低天数',20,'landMinDays'),(9,'股权每日最大兑换数量',10000,'maxPerDay'),(10,'股权GXB兑例比率',0.001,'EquityGXBRate'),(11,'股权价格',18.88,'equityPrice'),(12,'股权最高价',1,'equityPriceMax'),(13,'股权最低价',0.01,'equityPriceMin');
 /*!40000 ALTER TABLE `config` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-22 10:42:29
+-- Dump completed on 2021-05-16 12:56:29
