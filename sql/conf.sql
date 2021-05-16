d47b458f2decaae1d8743ff273e6604476c5ddf9
Al Zee <z@alz.ee>
2021-05-16 14:00:04 +0800

    conf.sql

--- sql/conf.sql
+++ sql/conf.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.7.33, for Linux (x86_64)
+-- MariaDB dump 10.19  Distrib 10.4.18-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: gxb
 -- ------------------------------------------------------
--- Server version	5.7.33-log
+-- Server version	10.4.18-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,29 +16,40 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `config`
+-- Table structure for table `conf`
 --
 
-DROP TABLE IF EXISTS `config`;
+DROP TABLE IF EXISTS `conf`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `config` (
+CREATE TABLE `conf` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `value` double NOT NULL,
-  `label` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `equity` int(11) NOT NULL,
+  `refer_reward` double NOT NULL,
+  `refer_reward2` double NOT NULL,
+  `refer_gxb` smallint(6) NOT NULL,
+  `mainland_min_price` double NOT NULL,
+  `land_min_price` double NOT NULL,
+  `mainland_min_days` smallint(6) NOT NULL,
+  `land_min_days` smallint(6) NOT NULL,
+  `max_per_day` int(11) NOT NULL,
+  `equity_gxbrate` double NOT NULL,
+  `equity_price` double NOT NULL,
+  `equity_price_max` double NOT NULL,
+  `equity_price_min` double NOT NULL,
+  `dividend_fund` double NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `config`
+-- Dumping data for table `conf`
 --
 
-LOCK TABLES `config` WRITE;
-/*!40000 ALTER TABLE `config` DISABLE KEYS */;
-INSERT INTO `config` VALUES (1,'股权总量',14000000000,'equity'),(2,'推广一级奖励',0.08,'referReward'),(3,'推广二级奖励',0.02,'referReward2'),(4,'推广GXB奖励',100,'referGXB'),(5,'主城起步价',1,'mainlandMinPrice'),(6,'分城起步价',0.05,'landMinPrice'),(7,'主城最低天数',1,'mainlandMinDays'),(8,'分城最低天数',20,'landMinDays'),(9,'股权每日最大兑换数量',10000,'maxPerDay'),(10,'股权GXB兑例比率',0.001,'EquityGXBRate'),(11,'股权价格',18.88,'equityPrice'),(12,'股权最高价',1,'equityPriceMax'),(13,'股权最低价',0.01,'equityPriceMin');
-/*!40000 ALTER TABLE `config` ENABLE KEYS */;
+LOCK TABLES `conf` WRITE;
+/*!40000 ALTER TABLE `conf` DISABLE KEYS */;
+INSERT INTO `conf` VALUES (1,99999,0.08,0.02,100,1,0.05,1,20,10000,0.001,18.88,1,0.01,100);
+/*!40000 ALTER TABLE `conf` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -50,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-16 12:56:29
+-- Dump completed on 2021-05-16 13:59:41
