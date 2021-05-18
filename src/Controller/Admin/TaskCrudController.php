8889318f10d7dcecd1a9374db3f69882519e9ba9
Al Zee <z@alz.ee>
2021-05-18 21:39:21 +0800

    task crud

--- src/Controller/Admin/TaskCrudController.php
+++ src/Controller/Admin/TaskCrudController.php
@@ -26,7 +26,7 @@ class TaskCrudController extends AbstractCrudController
     public function configureCrud(Crud $crud): Crud
     {
         return $crud
-            ->setSearchFields(['id', 'name', 'title', 'price', 'description', 'quantity', 'bidPosition', 'link', 'note', 'guides', 'reviews', 'workHours', 'reviewHours'])
+            ->setSearchFields(['id', 'name', 'title', 'price', 'description', 'quantity', 'link', 'note', 'guides', 'reviews', 'workHours', 'reviewHours'])
             ->setPaginatorPageSize(50);
     }
 
@@ -45,18 +45,12 @@ class TaskCrudController extends AbstractCrudController
         $title = TextField::new('title');
         $price = IntegerField::new('price');
         $description = TextareaField::new('description');
-        $prepaid = TextareaField::new('prepaid');
         $owner = AssociationField::new('owner');
         $platform = AssociationField::new('platform');
         $category = AssociationField::new('category');
         $tag = AssociationField::new('tag');
         $quantity = IntegerField::new('quantity');
         $applies = AssociationField::new('applies');
-        $showdays = Field::new('showdays');
-        $approvedays = Field::new('approvedays');
-        $applydays = Field::new('applydays');
-        $bidPosition = IntegerField::new('bidPosition');
-        $approved = BooleanField::new('approved');
         $id = IntegerField::new('id', 'ID');
         $date = DateTimeField::new('date');
         $link = TextField::new('link');
@@ -71,13 +65,13 @@ class TaskCrudController extends AbstractCrudController
         $status = AssociationField::new('status');
 
         if (Crud::PAGE_INDEX === $pageName) {
-            return [$id, $title, $price, $prepaid, $owner, $platform, $category, $tag, $quantity, $applies, $approved, $date];
+            return [$id, $title, $price, $owner, $platform, $category, $tag, $quantity, $applies, $date];
         } elseif (Crud::PAGE_DETAIL === $pageName) {
-            return [$id, $name, $title, $price, $description, $quantity, $date, $bidPosition, $link, $note, $guides, $reviews, $stickyUntil, $recommendUntil, $showUntil, $workHours, $reviewHours, $owner, $platform, $category, $tag, $applies, $status];
+            return [$id, $name, $title, $price, $description, $quantity, $date, $link, $note, $guides, $reviews, $stickyUntil, $recommendUntil, $showUntil, $workHours, $reviewHours, $owner, $platform, $category, $tag, $applies, $status];
         } elseif (Crud::PAGE_NEW === $pageName) {
-            return [$name, $title, $price, $description, $prepaid, $owner, $platform, $category, $tag, $quantity, $applies, $showdays, $approvedays, $applydays, $bidPosition, $approved];
+            return [$name, $title, $price, $description, $owner, $platform, $category, $tag, $quantity, $applies];
         } elseif (Crud::PAGE_EDIT === $pageName) {
-            return [$name, $title, $price, $description, $prepaid, $owner, $platform, $category, $tag, $quantity, $applies, $showdays, $approvedays, $applydays, $bidPosition, $approved];
+            return [$name, $title, $price, $description, $owner, $platform, $category, $tag, $quantity, $applies];
         }
     }
 }
