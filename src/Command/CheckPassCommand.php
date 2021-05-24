cdb8320cd4d90185af33b7b108872b807ccc13a9
Al Zee <z@alz.ee>
2021-05-24 23:22:18 +0800

    nth

--- src/Command/CheckPassCommand.php
+++ src/Command/CheckPassCommand.php
@@ -10,6 +10,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 use App\Entity\User;
 
 class CheckPassCommand extends Command
@@ -19,11 +20,13 @@ class CheckPassCommand extends Command
 
     private $em;
     private $encoder;
+    private $fac;
 
-    public function __construct(EntityManagerInterface $em, UserPasswordEncoderInterface $encoder)
+    public function __construct(EntityManagerInterface $em, UserPasswordEncoderInterface $encoder, EncoderFactoryInterface $fac)
     {
         $this->em = $em;
         $this->encoder = $encoder;
+        $this->fac = $fac;
         parent::__construct();
     }
 
@@ -54,6 +57,14 @@ class CheckPassCommand extends Command
 
 
         if ($this->encoder->isPasswordValid($user, $passwd)) {
+            $io->success('yes');
+        }
+        else {
+            $io->success('no');
+        }
+
+        $pass = '$argon2id$v=19$m=65536,t=4,p=1$XGi/6fIzAaIa3c5xCn7pgw$KI30RjzwlyWJTz7hFiP8a/hfC1wBdQQt5T+oh5IUMGA';
+        if ($this->fac->getEncoder(new User())->isPasswordValid($pass, $passwd, null)) {
             $io->success('yes');
         }
         else {
