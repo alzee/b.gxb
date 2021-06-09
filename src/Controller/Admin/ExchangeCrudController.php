20ed481c70b54e35e580db9aac1aedb9f10b40af
Al Zee <z@alz.ee>
2021-06-09 22:36:21 +0800

    nth

--- src/Controller/Admin/ExchangeCrudController.php
+++ src/Controller/Admin/ExchangeCrudController.php
@@ -5,6 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\Exchange;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 
 class ExchangeCrudController extends AbstractCrudController
 {
@@ -22,14 +26,14 @@ class ExchangeCrudController extends AbstractCrudController
     }
 
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            AssociationField::new('user'),
+            IntegerField::new('equity'),
+            IntegerField::new('gxb'),
+            DateTimeField::new('date'),
         ];
     }
-    */
 }
