84733cb158f74ead74c69a0acf27099bda7fdcb4
Al Zee <z@alz.ee>
2021-06-15 21:30:41 +0800

    so we can get changeset in entity eventlistener

--- src/EventListener/TaskUpdate.php
+++ src/EventListener/TaskUpdate.php
@@ -19,51 +19,56 @@ class TaskUpdate
     public function postUpdate(Task $task, LifecycleEventArgs $event): void
     {
         $em = $event->getEntityManager();
+        $uow = $em->getUnitOfWork();
+        $changeSet = $uow->getEntityChangeSet($task);
         $owner = $task->getOwner();
         $conf = $em->getRepository(Conf::class)->find(1);
+        $status = $task->getStatus()->getId();
 
-        if ($task->getStatus()->getId() == 2) {
-            $f = $task->getFinance();
-            $fee = $f->getFee();
-            $conf->setDividendFund($conf->getDividendFund() + $fee);
-            $f->setIsFund(true);
-            $em->flush();
-        }
+        if (isset($changeSet['status'])) {
+            if ($status == 2) {
+                $f = $task->getFinance();
+                $fee = $f->getFee();
+                $conf->setDividendFund($conf->getDividendFund() + $fee);
+                $f->setIsFund(true);
+                $em->flush();
+            }
 
-        if ($task->getStatus()->getId() == 4) {
-            $remain = $task->getRemain();
-            $price = $task->getPrice();
-            $amountLeft = $remain * $price;
+            if ($status == 4) {
+                $remain = $task->getRemain();
+                $price = $task->getPrice();
+                $amountLeft = $remain * $price;
 
-            $owner->setFrozen($owner->getFrozen() - $amountLeft);
-            $owner->setTopup($owner->getTopup() + $amountLeft);
+                $owner->setFrozen($owner->getFrozen() - $amountLeft);
+                $owner->setTopup($owner->getTopup() + $amountLeft);
 
-            $type = 56;
+                $type = 56;
 
-            $f = new Finance();
-            $f->setUser($owner);
-            $f->setAmount($amountLeft);
-            $f->setType($type);
-            $em->persist($f);
-            $em->flush();
-        }
+                $f = new Finance();
+                $f->setUser($owner);
+                $f->setAmount($amountLeft);
+                $f->setType($type);
+                $em->persist($f);
+                $em->flush();
+            }
 
-        if ($task->getStatus()->getId() == 5) {
-            $finance = $task->getFinance();
-            $amount = $finance->getAmount();
-            $fee = $finance->getFee();
+            if ($status == 5) {
+                $finance = $task->getFinance();
+                $amount = $finance->getAmount();
+                $fee = $finance->getFee();
 
-            $owner->setFrozen($owner->getFrozen() - $amount);
-            $owner->setTopup($owner->getTopup() + $amount + $fee);
+                $owner->setFrozen($owner->getFrozen() - $amount);
+                $owner->setTopup($owner->getTopup() + $amount + $fee);
 
-            $type = 61;
+                $type = 61;
 
-            $f = new Finance();
-            $f->setUser($owner);
-            $f->setAmount($amount);
-            $f->setType($type);
-            $em->persist($f);
-            $em->flush();
+                $f = new Finance();
+                $f->setUser($owner);
+                $f->setAmount($amount);
+                $f->setType($type);
+                $em->persist($f);
+                $em->flush();
+            }
         }
 
     }
